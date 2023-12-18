0ff2f6a6d631905a4ef6e246565858271d47f743
Saad Mazhar <42980243+notsaad@users.noreply.github.com>
2023-12-17 20:15:46 -0500

    Update day1part1.py

--- day1part1.py
+++ day1part1.py
@@ -1,30 +1,16 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
+filename = 'test-input.txt'
+answer = 0
 
-int main(){
-  std::string line;
-  std::ifstream file("input.txt");
-  int sum = 0;
-  std::vector<int> vector;
-  if (file.is_open()) {
-        while (getline(file, line)) {
-          for (char c : line){
-            if (isdigit(c)){
-              vector.push_back(c - '0');
-            }
-          }
-          if (vector.size() == 1){
-            sum += vector.at(0) + (vector.at(0) * 10);
-          }
-          else {
-            sum += (vector.at(0) * 10) + vector.back();
-          }
-          vector.clear();
-        }
-        file.close();
-    }
-  std::cout << sum << std::endl;
-  return 0;
-}
+with open(filename, 'r') as file:
+    line = file.readline()
+    while line:
+        stack = []
+        for char in line.strip():
+            if char.isdigit():
+                stack.append(char)
+        if len(stack) == 1:
+            answer += int(stack[0] + stack[0])
+        else:
+            answer += int(stack[0] + stack[len(stack) - 1])
+        line = file.readline()
+print(answer)
